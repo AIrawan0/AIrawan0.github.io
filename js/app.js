f1926b1b8546ceb72957efdd25a69bb91b8ed4e4
AIrawan0 <100456473+AIrawan0@users.noreply.github.com>
2024-04-03 14:25:16 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration:2600,
+    delay: 450,
+    resset: true
+});
+
+sr.reveal('.hero-text',{delay:200,origin:'top'});
+sr.reveal('h1',{delay:150,origin:'top'});
+sr.reveal('.contacts',{delay:300,origin:'left'});
+sr.reveal('.scroll-down',{delay:250,origin:'right'});
+sr.reveal('.option-card',{delay:150,origin:'top'});
